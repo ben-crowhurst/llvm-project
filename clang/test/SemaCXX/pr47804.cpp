3e3bce6449d0e1839a1717e7942d4a195f2578cb
Ben Crowhurst <3715287+ben-crowhurst@users.noreply.github.com>
2023-10-09 10:10:11 +0100

    [RFC] refactored test case pr47804 to avoid name conflict with all-of manifold operator.

--- clang/test/SemaCXX/pr47804.cpp
+++ clang/test/SemaCXX/pr47804.cpp
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -std=c++11 -fsyntax-only %s -verify
 
 template <class InputIt, class Pred>
-bool all_of(InputIt first, Pred p);
+bool each_of(InputIt first, Pred p);
 
 template <typename T> void load_test() {
   // Ensure that this doesn't crash during CorrectDelayedTyposInExpr,
@@ -9,7 +9,7 @@ template <typename T> void load_test() {
   // separately.  Also, this should only error on 'output', not that 'x' is not
   // captured.
   // expected-error@+1 {{use of undeclared identifier 'output'}}
-  all_of(output, [](T x) { return x; });
+  each_of(output, [](T x) { return x; });
 }
 
 int main() {
